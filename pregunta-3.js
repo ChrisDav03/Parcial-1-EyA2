384b3dcdf30f7f8f42067332813a9b96f67d4952
Christian <christianbur1248@gmail.com>
2023-08-31 14:17:46 -0500

    v2

--- pregunta-3.js
+++ pregunta-3.js
@@ -75,25 +75,45 @@ class LinkedList {
 }
 
 
-const mezclarListas =(lista1,lista2) => {
-    console.log(lista1)
-    const nuevaLista = [];
-    nuevaLista.push(lista1.head)
-    nuevaLista.push(lista2.head)
-    console.log(nuevaLista)
-    
+const mezclarListas = (lista1, lista2) => {
+    const nuevaLista = new LinkedList();
 
-    return nuevaLista
-}
+    let current1 = lista1.head;
+    let current2 = lista2.head;
+
+    while (current1 !== null && current2 !== null) {
+        if (current1.value < current2.value) {
+            nuevaLista.append(current1.value);
+            current1 = current1.next;
+        } else {
+            nuevaLista.append(current2.value);
+            current2 = current2.next;
+        }
+    }
+
+    while (current1 !== null) {
+        nuevaLista.append(current1.value);
+        current1 = current1.next;
+    }
+
+    while (current2 !== null) {
+        nuevaLista.append(current2.value);
+        current2 = current2.next;
+    }
+
+    return nuevaLista;
+};
 const miLista = new LinkedList();
 miLista.append(1)
 miLista.append(2)
 miLista.append(3)
+miLista.append(7)
+
 const miLista2 = new LinkedList();
 miLista2.append(4)
 miLista2.append(5)
 miLista2.append(6)
 
-console.log(miLista.head.value)
-mezclarListas(miLista, miLista2)
+
+console.log(mezclarListas(miLista, miLista2))
 
